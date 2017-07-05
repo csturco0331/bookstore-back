8e8cbd587619c54a093ceb1e5d61b56fed66e003
Cory Turco <csturco0331@yahoo.com>
2017-07-05 11:27:39 -0400

    Added a transactional repository. Also performed Maven project update

--- src/main/java/com/pluralsight/bookstore/model/Book.java
+++ src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -1,6 +1,6 @@
 package com.pluralsight.bookstore.model;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -37,5 +37,77 @@ public class Book {
 	private String imageUrl;
 	
 	private Language language;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Float getUnitCost() {
+		return unitCost;
+	}
+
+	public void setUnitCost(Float unitCost) {
+		this.unitCost = unitCost;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+
+	public Integer getNbOfPages() {
+		return nbOfPages;
+	}
+
+	public void setNbOfPages(Integer nbOfPages) {
+		this.nbOfPages = nbOfPages;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Language getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(Language language) {
+		this.language = language;
+	}
 	
 }
---
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
@@ -0,0 +1,45 @@
+package com.pluralsight.bookstore.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import com.pluralsight.bookstore.model.Book;
+
+@Transactional(SUPPORTS) //Used for ReadOnly transactions
+public class BookRepository {
+
+	@PersistenceContext(unitName = "bookStorePU")
+	private EntityManager em;
+	
+	public Book find(Long id) {
+		return em.find(Book.class, id);
+	}
+	
+	@Transactional(REQUIRED) //Used for WriteOnly transactions
+	public Book create(Book book) {
+		em.persist(book);
+		return book;
+	}
+	
+	@Transactional(REQUIRED)
+	public void delete(Long id) {
+		em.remove(em.getReference(Book.class, id));	
+	}
+	
+	public List<Book> findAll() {
+		TypedQuery<Book> query = em.createQuery("select b from Book b order by b.title desc", Book.class);
+		return query.getResultList();
+	}
+	
+	public Long countAll() {
+		TypedQuery<Long> query = em.createQuery("select count(b) from Book b", Long.class);
+		return query.getSingleResult();
+	}
+}
